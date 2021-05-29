091c0af908ab186d9cdf5bb157a7889763b81e6f
raulrd3 <hi@raulrodrigueziii.com>
2021-05-29 13:08:27 -0500

    Ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,35 @@ class HomePage extends StatelessWidget {
         title: Text("Awesome App"),
       ),
       body: Container(
-        child: Text("Hi Flutter"),
+        color: Colors.teal,
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            ),
+          ],
+        ),
       ),
     );
   }
